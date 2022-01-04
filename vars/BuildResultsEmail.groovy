b944cf7a5e5225ab595e9dc38e7fabff787a87b8
dylanmeh <89469261+dylanmeh@users.noreply.github.com>
2022-01-03 20:44:09 -0500

    Create BuildResultsEmail.groovy

---
+++ vars/BuildResultsEmail.groovy
@@ -0,0 +1,8 @@
+def call(String buildResultsEmail) {
+  emailext (
+  subject: "${buildResultsEmail}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
+  body: """${buildResultsEmail}: Job '${JOB_NAME} [${BUILD_NUMBER}]':
+  Check console output at ${BUILD_URL}""",
+  to: 'dylan.mehmedovic@concanon.com'
+  )
+}
