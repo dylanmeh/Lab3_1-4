7079b2a607aff229880fc9908137fb1d04c4ff26
dylanmeh <dylan.mehmedovic@concanon.com>
2022-01-07 10:58:13 -0500

    removed method definiton in line 3

--- vars/getTriggerCauseEvent.groovy
+++ vars/getTriggerCauseEvent.groovy
@@ -1,10 +1,7 @@
 def call() {
-
-    String getTriggerCauseEvent() {
         def buildCauseInfo = currentBuild.getBuildCauses("com.cloudbees.jenkins.plugins.pipeline.events.EventTriggerCause")
         if (buildCauseInfo)  {
             def unitTestEnable = buildCauseInfo.event.unitTestEnable
             return unitTestEnable
-        }
-    }        
+        }        
 }
