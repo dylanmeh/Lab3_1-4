205582f4c1cee484dd9843d146fb2d74ea695143
dylanmeh <dylan.mehmedovic@concanon.com>
2022-01-07 17:22:33 -0500

    ..

--- vars/getTriggerCauseEvent.groovy
+++ vars/getTriggerCauseEvent.groovy
@@ -1,4 +1,4 @@
-def getTriggerCauseEvent() {
+String getTriggerCauseEvent() {
     def buildCauseInfo = currentBuild.getBuildCauses("com.cloudbees.jenkins.plugins.pipeline.events.EventTriggerCause")
     if (buildCauseInfo && buildCauseInfo[0])  {
         String artifactId = buildCauseInfo[0].event.environment
