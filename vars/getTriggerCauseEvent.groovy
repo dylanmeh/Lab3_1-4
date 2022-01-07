d593d4f4b7ed594bdbef0ebba1dd7f69422b2091
dylanmeh <dylan.mehmedovic@concanon.com>
2022-01-07 17:44:18 -0500

    adding println in code

--- vars/getTriggerCauseEvent.groovy
+++ vars/getTriggerCauseEvent.groovy
@@ -1,6 +1,6 @@
 String getTriggerCauseEvent() {
     def buildCauseInfo = currentBuild.getBuildCauses("com.cloudbees.jenkins.plugins.pipeline.events.EventTriggerCause")
-    echo "trace code"
+    println "trace code"
     if (buildCauseInfo && buildCauseInfo[0])  {
         String artifactId = buildCauseInfo[0].event.environment
         return artifactId
