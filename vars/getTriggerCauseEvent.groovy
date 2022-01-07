7dc6aa2d413323ef3c2d6df3de4d616bb546b97b
dylanmeh <dylan.mehmedovic@concanon.com>
2022-01-07 16:54:34 -0500

    remove call() {} block in getTriggerCauseEvent function

--- vars/getTriggerCauseEvent.groovy
+++ vars/getTriggerCauseEvent.groovy
@@ -1,10 +1,8 @@
-def call() {
-    String getTriggerCauseEvent() {
-        def buildCauseInfo = currentBuild.getBuildCauses("com.cloudbees.jenkins.plugins.pipeline.events.EventTriggerCause")
-        if (buildCauseInfo && buildCauseInfo[0])  {
-            String artifactId = buildCauseInfo[0].event.environment
-            return artifactId
-        }
-        return "N/A"
+String getTriggerCauseEvent() {
+    def buildCauseInfo = currentBuild.getBuildCauses("com.cloudbees.jenkins.plugins.pipeline.events.EventTriggerCause")
+    if (buildCauseInfo && buildCauseInfo[0])  {
+        String artifactId = buildCauseInfo[0].event.environment
+        return artifactId
     }
+    return "N/A"
 }
