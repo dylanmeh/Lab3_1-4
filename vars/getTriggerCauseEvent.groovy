9b8311bd1d51d6f70b7d0bc8c9a04d25cbe424d7
dylanmeh <dylan.mehmedovic@concanon.com>
2022-01-07 17:41:12 -0500

    tracing code

--- vars/getTriggerCauseEvent.groovy
+++ vars/getTriggerCauseEvent.groovy
@@ -1,5 +1,6 @@
 String getTriggerCauseEvent() {
     def buildCauseInfo = currentBuild.getBuildCauses("com.cloudbees.jenkins.plugins.pipeline.events.EventTriggerCause")
+    echo "trace code"
     if (buildCauseInfo && buildCauseInfo[0])  {
         String artifactId = buildCauseInfo[0].event.environment
         return artifactId
