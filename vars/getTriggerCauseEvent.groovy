a9de43851bac7db42a61a9f6593f8fbf0647b42d
dylanmeh <dylan.mehmedovic@concanon.com>
2022-01-10 09:59:53 -0500

    adding debugging statement to confitional statement test if i'm in that block

--- vars/getTriggerCauseEvent.groovy
+++ vars/getTriggerCauseEvent.groovy
@@ -2,8 +2,10 @@ String getTriggerCauseEvent() {
     def buildCauseInfo = currentBuild.getBuildCauses("com.cloudbees.jenkins.plugins.pipeline.events.EventTriggerCause")
     println "retrieving payload"
     if (buildCauseInfo && buildCauseInfo[0])  {
+        println "inside of conditional statement"
         String artifactId = buildCauseInfo[0].event.environment
         return artifactId
+
     }
     return "N/A"
 }
