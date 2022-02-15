8d34d4a8069398018263f7fa9b43ddb39d2ef013
dylanmeh <dylan.mehmedovic@concanon.com>
2022-02-15 12:35:47 -0500

    adding groovy function printing out repo branch and repo url in webhook payload

---
+++ vars/getWebhookTriggerCauseEvent.groovy
@@ -0,0 +1,14 @@
+String getTriggerCauseEvent() {
+    def buildCauseInfo = currentBuild.getBuildCauses("com.cloudbees.jenkins.plugins.pipeline.events.EventTriggerCause")
+    println "retrieving payload"
+    if (buildCauseInfo && buildCauseInfo[0])  {
+        println "inside of conditional statement"
+        String webhook_branch = buildCauseInfo[0].event.ref
+        String repo_url = buildCauseInfo[0].event.repository.url
+        println "${webhook_branch}"
+        println "${repo_url}"
+        return webhook_branch
+        return repo_url         
+    }
+    return "N/A"
+}    
